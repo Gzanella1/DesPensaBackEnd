36b5bee4ce0938a2701331babd9aff69e04c42a2
Gzanella1 <giovanizanelladamaia123@gmail.com>
2025-10-16 10:58:48 -0300

    first commit

---
+++ src/test/java/com/api/desPensa/DesPensaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.desPensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesPensaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
