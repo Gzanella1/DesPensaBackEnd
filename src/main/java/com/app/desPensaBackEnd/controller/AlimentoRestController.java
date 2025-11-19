028a7e2c2ac86caa9c48800316d430e742da54fa
cbrito992 <crlosjob92@gmail.com>
2025-11-19 10:52:57 -0300

    ajustes AlimentoRestController

--- src/main/java/com/app/desPensaBackEnd/controller/AlimentoRestController.java
+++ src/main/java/com/app/desPensaBackEnd/controller/AlimentoRestController.java
@@ -1,51 +1,53 @@
 package com.app.desPensaBackEnd.controller;
 
+import com.app.desPensaBackEnd.model.dto.AlimentoDTO;
 import com.app.desPensaBackEnd.model.dto.UsuarioDTO;
+import com.app.desPensaBackEnd.view.services.AlimentoService;
 import com.app.desPensaBackEnd.view.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Set;
 
+@RestController
+@RequestMapping("/alimento")
 public class AlimentoRestController {
-	}
+	
 
 	@Autowired
 	private AlimentoService alimentoService;
 
-// GET - Listar Todos
-
-@GetMapping
-public List<Alimento> listartodos( ) {
-	return alimentoService.listarTodos ( );{
-}
+	// GET - Listar Todos
 	
-// GET - Buscar por ID
-
-@GetMapping("/{id}")
-public Alimento buscarPorId(@PathVariable Long id) {
-	return alimentoService.buscarPorId(id);
-}
-
-// POST - criar novo alimento
-@PostMapping
-public Alimento criar(@RequestBody Alimento alimento) {
-    return alimentoService.salvar(alimento);
-}
-
-// PUT - atualizar alimento
-@PutMapping("/{id}")
-public Alimento atualizar(
-        @PathVariable Long id,
-        @RequestBody Alimento alimentoAtualizado
-) {
-    return alimentoService.atualizar(id, alimentoAtualizado);
-}
-
-// DELETE - remover alimento
-@DeleteMapping("/{id}")
-public void deletar(@PathVariable Long id) {
-    alimentoService.deletar(id);
-}
+    @GetMapping("/buscaAlimento")
+	public List<AlimentoDTO> listartodos( ) {
+		return alimentoService.listarTodos ( );
+	}
+		
+	// GET - Buscar por ID
+	
+	@GetMapping("/{id}")
+	public AlimentoDTO buscarPorId(@PathVariable Long id) {
+		return alimentoService.buscarPorId(id);
+	}
+	
+	// POST - criar novo alimento
+	@PostMapping
+	public AlimentoDTO criar(@RequestBody AlimentoDTO alimento) {
+	    return alimentoService.salvar(alimento);
+	}
+	
+	// PUT - atualizar alimento
+	@PutMapping("/{id}")
+	public AlimentoDTO atualizar(   @PathVariable Long id, @RequestBody AlimentoDTO alimentoAtualizado) {
+	    return alimentoService.atualizar(id, alimentoAtualizado);
+	}
+	
+	// DELETE - remover alimento
+	@DeleteMapping("/{id}")
+	public void deletar(@PathVariable Long id) {
+	    alimentoService.deletar(id);
+	}
 }
--- src/main/java/com/app/desPensaBackEnd/view/services/AlimentoService.java
+++ src/main/java/com/app/desPensaBackEnd/view/services/AlimentoService.java
@@ -2,15 +2,21 @@ package com.app.desPensaBackEnd.view.services;
 
 import com.app.desPensaBackEnd.enums.CategoriaAlimento;
 import com.app.desPensaBackEnd.model.dto.AlimentoDTO;
+import com.app.desPensaBackEnd.model.dto.UsuarioDTO;
 import com.app.desPensaBackEnd.model.entity.AlimentoEntity;
 import com.app.desPensaBackEnd.model.entity.EstoqueEntity;
 import com.app.desPensaBackEnd.view.repository.AlimentoRepository;
 import jakarta.persistence.*;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class AlimentoService {
@@ -19,9 +25,17 @@ public class AlimentoService {
     private AlimentoRepository alimentoRepository;
 
 
-    // buscar todos alimentos
-    //private List<AlimentoDTO> buscaTodosAlimentos(){
 
-    //}
+
+    public List<AlimentoDTO> listarTodos() {
+        return alimentoRepository.findAll().stream().map(aE -> {
+        	
+        	AlimentoDTO dto = new AlimentoDTO();
+            BeanUtils.copyProperties(aE, dto);
+
+               return dto;
+        }).collect(Collectors.toList());
+    }
+
 
 }
