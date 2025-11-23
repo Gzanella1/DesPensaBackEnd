11c0305dcb9ef62b9a4e1498af6602afdc340389
Gabriel-rossbach <rossbachgabriel@gmail.com>
2025-11-23 09:08:08 -0300

    O cadastro de endereço está funcionando agora, mas se alguém quiser testar, precisa alterar a senha para a sua no banco de dados, no arquivo application.properties.

--- src/main/java/com/app/desPensaBackEnd/model/entity/EnderecoEntity.java
+++ src/main/java/com/app/desPensaBackEnd/model/entity/EnderecoEntity.java
@@ -1,9 +1,7 @@
 package com.app.desPensaBackEnd.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "tb_endereco")
 public class EnderecoEntity {
@@ -13,25 +11,49 @@ public class EnderecoEntity {
     @Column(name = "id_endereco")
     private Long idEndereco;
 
-    @Column(name = "numero", nullable = false)
+    @Column(nullable = false)
     private String numero;
 
-    @Column(name = "complemento", length = 100)
     private String complemento;
 
-    @Column(name = "bairro", nullable = false, length = 100)
+    @Column(nullable = false, length = 100)
     private String bairro;
 
-    @Column(name = "cidade", nullable = false, length = 100)
+    @Column(nullable = false, length = 100)
     private String cidade;
 
-    @Column(name = "estado", nullable = false, length = 50)
+    @Column(nullable = false, length = 50)
     private String estado;
 
-    @Column(name = "cep", nullable = false, length = 10)
+    @Column(nullable = false, length = 10)
     private String cep;
 
-    @OneToOne(mappedBy = "endereco")
+    @OneToOne
+    @JoinColumn(name = "id_instituicao")
     private InstituicaoEntity instituicao;
 
+    // GETTERS E SETTERS
+    public Long getIdEndereco() { return idEndereco; }
+    public void setIdEndereco(Long idEndereco) { this.idEndereco = idEndereco; }
+
+    public String getNumero() { return numero; }
+    public void setNumero(String numero) { this.numero = numero; }
+
+    public String getComplemento() { return complemento; }
+    public void setComplemento(String complemento) { this.complemento = complemento; }
+
+    public String getBairro() { return bairro; }
+    public void setBairro(String bairro) { this.bairro = bairro; }
+
+    public String getCidade() { return cidade; }
+    public void setCidade(String cidade) { this.cidade = cidade; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public String getCep() { return cep; }
+    public void setCep(String cep) { this.cep = cep; }
+
+    public InstituicaoEntity getInstituicao() { return instituicao; }
+    public void setInstituicao(InstituicaoEntity instituicao) { this.instituicao = instituicao; }
 }
